6ed0eae18864186d0e12c75cb9cf8d6b6002a433
steve <ssaanen@gmail.com>
2013-05-07 12:16:49 +0200

    added base jq plugin setup

---
+++ jquery.tagfiltering.js
@@ -0,0 +1,15 @@
+(function( $ ) {
+	$.fn.tagfiltering = function( data, options ) {
+		// Create some defaults, extending them with any options that were provided
+		var settings = $.extend({
+
+		}, options);
+
+		// constants
+		var $data = list;
+		var $tags = this;
+
+
+		return this;
+	};
+})( jQuery );
