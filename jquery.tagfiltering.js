b8f89fd8c6678fe0e2b5af93090660ec907cb132
steve <ssaanen@gmail.com>
2013-05-07 16:33:51 +0200

    added actual filtering

--- jquery.tagfiltering.js
+++ jquery.tagfiltering.js
@@ -1,25 +1,44 @@
 (function( $ ) {
-	$.fn.tagfiltering = function( list, options ) {
+	$.fn.tagfiltering = function(list, options) {
 		// Create some defaults, extending them with any options that were provided
 		var settings = $.extend({
 
 		}, options);
 
-		// constants
-		var $data = list;
-		var $tags = $(this).children();
-
 		try {
-			// precondition checks
-			if (!$data.length)		throw "no valid target list to filter";
-			if (!$tags.length)		throw "called on invalid element (should be html list of tags)"
+			// constants & preconditions
+			if (!(list instanceof jQuery))	throw "list of items to filter must be specified as jQuery object";
+			var $items = list.children();
+			if (!$items.length)				throw "no valid target items to filter";
+			if (!(this instanceof jQuery))	throw "list of tags to use as filter must be specified as jQuery object";
+			var $tags = this.children();
+			if (!$tags.length)				throw "called on invalid element (should be non-empty html list of tags)"
 
+			// bind to click
 			$tags.click(function(event) {
 				event.preventDefault();
 
 				var target_tag = $(this).attr("data-tag");
+				get_filtered_data(target_tag);
 			});
 
+
+			// actual filtering
+			function get_filtered_data(target_tag) {
+				if (!target_tag)			throw "no tag specified";
+				console.log(target_tag);
+
+				var $filtered_items = $items.clone().filter(function() {
+					var tags = $(this).attr("data-tags").split(", ?");
+					//console.log(tags);
+
+					// it doesn't have the tag, oh noo
+					if ($.inArray(target_tag, tags) == -1)
+						return false;
+					else
+						return true;
+				});
+			}
 
 
 		}
